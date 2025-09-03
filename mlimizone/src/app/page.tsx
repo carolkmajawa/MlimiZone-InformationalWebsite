1f228b5635a80b67b1242181f941a581cca4dbc5
carol <khembocarol@gmail.com>
2025-09-03 18:20:45 +0300

    how ir works files commited

---
+++ mlimizone/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import FeaturesPage from "./HowItWorks"
+
+export default function Home() {
+  return (
+           <div>
+            <FeaturesPage/>
+           </div>
+  );
+}
